af214cea35f9fcab76876dc5e5f6286b793982e2
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-17 18:25:33 +0000

    Hotfix

--- functions3.c
+++ functions3.c
@@ -23,7 +23,7 @@ void _pchar(stack_t **stack, unsigned int lin_num)
 	if (list.Size)
 	{
 		if (list.Head->n > 0 && list.Head->n <= 127)
-			printf("%c\n", list.Head->n);
+			putchar(list.Head->n);
 		else
 		{
 			fprintf(stderr, "L%d: can't pchar, value out of range\n", lin_num);
