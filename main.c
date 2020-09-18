5f7ae9c9080e5220c89ae350f1dcba407a0a12ef
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2020-09-18 03:30:09 +0000

    Hotfix

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-#define s "L%d: unknown instruction %s\n"
+#define s "L%lu: unknown instruction %s\n"
 
 /**
  * Destroy - Function destroy
@@ -14,19 +14,42 @@ void Destroy(List *list)
 }
 
 /**
-* main - function to passed file such an arguments in interpreter
-* @argc: number of arguments
-* @argv: arguments
-* Return: Exit sucess if program is in the end
-*/
+ * stack_or_queue - Implement the stack and queue opcodes.
+ * @string: pointar string character.
+ * Return: Returns 1 if 0 matches otherwise
+ */
+
+int stack_or_queue(char *string)
+{
+	if (string)
+		if (strcmp("queue", list.inst_oper[0]) == 0)
+		{
+			list.State = 0;
+			return (1);
+		}
+		else if (strcmp("stack", list.inst_oper[0]) == 0)
+		{
+			list.State = 1;
+			return (1);
+		}
+		else
+			return (0);
+	else
+		return (0);
+}
+
+/**
+ * main - function to passed file such an arguments in interpreter
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Exit sucess if program is in the end
+ */
 
 int main(int argc, char *argv[])
 {
 	FILE *fd;
-	char *line = NULL;
-	size_t len = 0, line_num;
-	ssize_t nread;
-
+	char *line = NULL, *string = NULL;
+	size_t len = 0, line_num = 1;
 	void (*Pointer_Function)(stack_t **, unsigned int);
 
 	if (argc != 2)
@@ -34,15 +57,20 @@ int main(int argc, char *argv[])
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
-	list.Fd = fd;
-	line_num = 1;
-	while ((nread = getline(&line, &len, fd)) != -1)
+	list.Fd = fd, list.State = 1;
+	while (getline(&line, &len, fd) != EOF)
 	{
 		list.Solve_line = line;
 		(list.inst_oper)[0] = strtok(line, "\t\n ");
-		(list.inst_oper)[1] = strtok(NULL, "\t\n ");
-		if ((list.inst_oper[0] && list.inst_oper[0][0] == '#'))
+
+		if (list.inst_oper[0] && list.inst_oper[0][0] == '#')
+		{
+			free(line), line = list.inst_oper[0] = NULL;
 			continue;
+		}
+		if (stack_or_queue(list.inst_oper[0]))
+			continue;
+		(list.inst_oper)[1] = strtok(NULL, "\t\n ");
 		if (list.inst_oper[0])
 		{
 			Pointer_Function = get_op((list.inst_oper)[0]);
@@ -50,14 +78,12 @@ int main(int argc, char *argv[])
 				Pointer_Function(NULL, line_num);
 			else
 			{
-				char *string =  list.inst_oper[0];
-
-				fprintf(stderr, s, (int)line_num, string);
-				free(line), fclose(list.Fd);
-				Destroy(&list), exit(EXIT_FAILURE);
+				string =  list.inst_oper[0];
+				fprintf(stderr, s, line_num, string);
+				free(line), fclose(list.Fd), Destroy(&list), exit(EXIT_FAILURE);
 			}
 		}
-		line_num++, free(line),	line = NULL, len  = 0;
+		line_num++, free(line),line = NULL, len  = 0;
 	}
 	fclose(fd), free(line), Destroy(&list);
 	return (0);
